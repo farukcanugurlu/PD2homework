6740ec93b1049e830c9601d95777cac6e88be55b
Farukcan Uğurlu <101269959+iubireStoned@users.noreply.github.com>
2022-05-17 11:45:10 +0300

    soru6

---
+++ soru6.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node {
+	int num;
+	Node* next;
+};
+
+typedef struct Node Node;
+
+Node* addEvenToTheFirstOddToTheEnd() {
+	Node* head = NULL;
+	Node* last = NULL;
+
+	int inputNum = 0;
+
+	printf("Sayi girebilirsiniz veya -1 yazarak cikabilirsiniz ");
+	scanf("%d", &inputNum);
+
+	while (inputNum != -1) {
+
+		Node* tempNode = (Node*)malloc(sizeof(Node));
+		tempNode->num = inputNum;
+		tempNode->next = NULL;
+
+		if (inputNum % 2 == 1) {
+			if (head == NULL) {
+				head = tempNode;
+				last = head;
+			}
+			else {
+				last->next = tempNode;
+				last = tempNode;
+			}
+		}
+		else {
+			if (head == NULL) {
+				head = tempNode;
+				last = head;
+			}
+			else {
+				tempNode->next = head;
+				head = tempNode;
+			}
+		}
+
+		printf("Sayi girebilirsiniz veya -1 yazarak cikabilirsiniz ");
+		scanf("%d", &inputNum);
+	}
+	return head;
+}
+
+int main() {
+
+	Node* head = NULL;
+	head = addEvenToTheFirstOddToTheEnd();
+	while (head != NULL)
+	{
+		printf("%d\n", head->num);
+		head = head->next;
+	}
+}
